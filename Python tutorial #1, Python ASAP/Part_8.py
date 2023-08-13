29ae3c39d28cbe8df7a47a8f321b18a37ae7cf81
dev-lovi <santiagolovisotto@gmail.com>
2023-08-12 23:57:27 -0300

    Day 2 of python everyday?? let's go

---
+++ Python tutorial #1, Python ASAP/Part_8.py
@@ -0,0 +1,12 @@
+    #CHAINED CONDITIONALS
+x = 7
+y = 8
+z = 0
+
+result1 = x == y
+result2 = y > x
+result3 = z < x + 2
+
+result4 = result1 or result2
+
+print(result4)
---
+++ Python tutorial #1, Python ASAP/Part_9.py
@@ -0,0 +1,13 @@
+    #IF / ELSE / ELIF
+x = input('Name: ')
+
+if x == 'Tim':
+    print('You are great!')
+elif x == 'Joe':
+    print('Bye joe')
+elif x == 'Aron':
+    print('Bye Aron')
+else:
+    print('No')
+
+print('Always do this')
