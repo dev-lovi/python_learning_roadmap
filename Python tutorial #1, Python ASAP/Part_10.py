598cc5f5fe7aa5906dc7cd486425ced36608c143
dev-lovi <santiagolovisotto@gmail.com>
2023-08-13 21:31:57 -0300

    Day 3 of python everyday?? let's go

---
+++ Python tutorial #1, Python ASAP/Part_10.py
@@ -0,0 +1,7 @@
+    #LIST / TUPLES
+x = [4, True, 'hi']
+#x.append([4, 5, 5, 6])
+#x.extend([4, 5, 5, 6])
+#print(x.pop())
+#print(x.pop(0))
+print(x)
---
+++ Python tutorial #1, Python ASAP/Part_11.py
@@ -0,0 +1,7 @@
+    #FOR LOOPS
+for i in range(10, -10, -2):
+    print(i)
+
+#stop 
+#start, stop
+#start, stop, step 
