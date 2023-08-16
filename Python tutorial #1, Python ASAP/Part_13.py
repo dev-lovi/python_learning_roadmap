22adb5b85115986da1983905b11a98ebed117ca8
dev-lovi <santiagolovisotto@gmail.com>
2023-08-15 23:50:11 -0300

    Day 5 of python everyday?? let's go

---
+++ Python tutorial #1, Python ASAP/Part_13.py
@@ -0,0 +1,8 @@
+    #While Loops
+x = [3, 42, 3, 4, 2, 4]
+
+i = 0
+while i < 0:
+    print('run')
+    i += 1
+    break
---
+++ Python tutorial #1, Python ASAP/Part_14.py
@@ -0,0 +1,9 @@
+    #Slice operator
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+y = ['hi', 'hello', 'hola', 'chau']
+s = ['hello']
+
+sliced = x[0:4:2]
+#sliced = x[start:stop:step]
+
+print(sliced)
