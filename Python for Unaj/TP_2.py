e443c093dc1659cc00cf225636901bb6dc44747c
dev-lovi <santiagolovisotto@gmail.com>
2023-09-10 23:28:09 -0300

    Day 31

---
+++ Python for Unaj/TP_2.py
@@ -0,0 +1,17 @@
+#EJERCICIOS A ENTREGAR TP N° 2
+
+#Pedir datos de 4 productos comprados, con su cantidad(entero) y precio unitario(decimal).
+#Mostrar cuánto se gasta por cada producto y el total gastado
+#Imprimir por cada producto un mensaje que muestre los datos, por ejemplo con dos   productos, el resultado seria:
+
+#Productos comprados:
+#Nombre: nombre_producto1 Precio: precio_producto1
+#Nombre: nombre_producto2 Precio: precio_producto2
+#******************************Total gastado: total
+
+#Imprimir las iniciales de cada producto
+#Imprimir el ultimo caracter de cada producto
+#Indicar para cada variable usada el tipo de dato de la variable.
+#Indicar para cada variable usada el valor almacenado con el que queda al finalizar el programa.
+#Definir dos condiciones que usen los datos de los productos, mostrando el resultado de combinar ambas condiciones en un operador lógico.
+#Resolver el mismo ejercicio pero ahora use listas o tuplas de acuerdo a su criterio. Recordar las diferentes formas de agregar elementos en una lista.
