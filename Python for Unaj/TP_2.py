56fee7a4216d4dc7f5889966ac4601edee4c1802
dev-lovi <santiagolovisotto@gmail.com>
2023-09-11 23:29:33 -0300

    Day 31

--- Python for Unaj/TP_2.py
+++ Python for Unaj/TP_2.py
@@ -15,3 +15,31 @@
 #Indicar para cada variable usada el valor almacenado con el que queda al finalizar el programa.
 #Definir dos condiciones que usen los datos de los productos, mostrando el resultado de combinar ambas condiciones en un operador lógico.
 #Resolver el mismo ejercicio pero ahora use listas o tuplas de acuerdo a su criterio. Recordar las diferentes formas de agregar elementos en una lista.
+
+
+def pregunta_inicial():
+    producto_1 = input("Ingrese su producto 1: ")
+    cant_producto_1 = int(input("Ingrese la cantidad de producto 1: "))
+    precio_producto_1 = float(input("Ingrese el precio de producto 1: "))
+
+    producto_2 = input("Ingrese su producto 2: ")
+    cant_producto_2 = int(input("Ingrese la cantidad de producto 2: "))
+    precio_producto_2 = float(input("Ingrese el precio de producto 2: "))
+
+    producto_3 = input("Ingrese su producto 3: ")
+    cant_producto_3 = int(input("Ingrese la cantidad de producto 3: "))
+    precio_producto_3 = float(input("Ingrese el precio de producto 3: "))
+
+    producto_4 = input("Ingrese su producto 4: ")
+    cant_producto_4 = int(input("Ingrese la cantidad de producto 4: "))
+    precio_producto_4 = float(input("Ingrese el precio de producto 4: "))
+
+    costo = (cant_producto_1 * precio_producto_1 + cant_producto_2 * precio_producto_2 + cant_producto_3 * precio_producto_3 + cant_producto_4 * precio_producto_4)
+
+    return costo
+
+costo = pregunta_inicial()
+
+print("El costo total es:", costo)
+
+
