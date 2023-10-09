655554ed848d79dcc02939f911960a9a04bb7b6a
dev-lovi <santiagolovisotto@gmail.com>
2023-10-08 23:37:50 -0300

    Day 59

--- Exercism_exercises/two-sum_leetcode.py
+++ Exercism_exercises/two-sum_leetcode.py
@@ -20,3 +20,9 @@
 #The space required does not depend on the size of the input array, so only constant space is used.
 
 #Code:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                  return [i, j]    
